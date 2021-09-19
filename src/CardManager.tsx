226459fb196f85d0ecf8683cee6fc549bf7e6d73
Jonathan Hall <jonathan.edward.hall@gmail.com>
2021-09-18 19:14:05 -0500

    Add buttons, styles, and hooks to CardManager.

--- src/CardManager.tsx
+++ src/CardManager.tsx
@@ -1,10 +1,12 @@
+import React, { useState, useEffect } from 'react';
 import './CardManager.css';
 import Deck from './Deck';
 import Hand from './Hand';
 import Played from './Played';
+import { shuffle } from './helpers.js';
 
 /*
-interface DeckManagerProps {
+interface CardManagerProps {
   deck: number[],
   discard: number[],
   hand: number[]
@@ -12,11 +14,32 @@ interface DeckManagerProps {
 */
 
 function CardManager() {
+  /**
+   * useState returns a pair: the current state value and a function to update it.
+   */
+  const [deck, setDeck] = useState([3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7]);
+  useEffect(() => {
+    setDeck(shuffle(deck));
+  }, [deck]);
+
+  const [hand, setHand] = useState([]);
+  const [played, setPlayed] = useState([]);
+
+  function handleDrawClick() {
+    let newHand = [];
+
+  }
+
   return (
     <div className="CardManager">
-      <Deck size={15} />
-      <Hand values={[5, 3, 4, 2]} />
-      <Played values={[4, 3, 2]} />
+      <div className="CardManager-buttons">
+        <button onClick={handleDrawClick}>Draw</button>
+      </div>
+      <div className="CardManager-cards">
+        <Deck size={deck.length} />
+        <Hand values={hand} />
+        <Played values={played} />
+      </div>
     </div>
   );
 }
